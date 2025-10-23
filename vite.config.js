4543ce809d52fd7afa531fe68405820a42822640
rolando <rolomc2016@gmail.com>
2025-10-22 22:11:53 -0500

    se agrega base

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/redes/', // 👈 nombre exacto del repositorio en GitHub
 })
